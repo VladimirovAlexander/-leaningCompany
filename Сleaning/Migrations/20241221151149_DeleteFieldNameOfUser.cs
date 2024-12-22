37a4b1451707fd81753b1e0deebff9c4801d98ce
Александр Владимиров <a.v.vladimirov111@gmail.com>
2024-12-22 13:57:32 +0300

    Добавление новой миграции DeleteFieldNameOfUser

---
+++ Сleaning/Migrations/20241221151149_DeleteFieldNameOfUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Сleaning.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteFieldNameOfUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
